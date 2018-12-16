0693b77df5fdd7c986df61705dc7788b12b9cd35
jordb22 <brault.jordon@me.com>
2018-12-15 21:21:12 -0500

    New Message

--- bot.js
+++ bot.js
@@ -6,9 +6,8 @@ bot.on("ready", () => {
 });
 
 bot.on("message", message => {
-  if(message.content.indexOf('Tier 5 Egg') > -1) {
-    let role1 = message.channel.guild.roles.find('name', 'Lv5Raid');
-    message.channel.send(`Hey! A ${role1} Egg is nearby!`);
+  if(message.content.indexOf('go in') > -1) {
+    message.channel.send(`Hey! Use The Raid Channel`);
   }
 });
 
