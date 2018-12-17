68aad43687ad5878888dd543f9f52ac3864839f7
jordb22 <brault.jordon@me.com>
2018-12-17 13:37:50 -0500

    Removed Embed

--- bot.js
+++ bot.js
@@ -31,36 +31,4 @@ bot.on("message", message => {
   }
 });
 
-bot.on("message", message => {
-  if(message.content.toLowerCase().indexOf('Pyre Badge') > -1) {
-    message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: bot.user.username,
-      icon_url: bot.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: bot.user.avatarURL,
-      text: "© Example"
-    }
-  }
-});
-
 bot.login(process.env.BOT_TOKEN);
