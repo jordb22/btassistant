b1062e6b3afc876c023ef5c8f188f0663d763780
jordb22 <brault.jordon@me.com>
2018-12-17 16:48:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,18 +32,18 @@ bot.on("message", message => {
 });
 
 bot.on("message", message => {
-  if(message.content.toLowerCase().indexOf('pyre') > -1) {
+   if(message.content.toLowerCase().indexOf('quarry') > -1) {
     message.channel.send({embed: {
-      color: 10038562,
-      title: "Pyre Badge - Information",
-      description: "Earn the Pyre Badge by defeating the Fire/Normal Frontier Brain in their Frontier Challenge!",
+      color: 12745742,
+      title: "Quarry Badge - Information",
+      description: "Earn the Quarry Badge by defeating the Rock/Ground Frontier Brain in their Frontier Challenge!",
       fields: [{
         name: "Current Frontier Brain",
-        value: "Jamie / @jojopride"
+        value: "George | @Yorgie18"
       },
       {
         name: "Frontier Challenge",
-        value: "To Be Announced."
+        value: "Great League Match | To Be Announced."
       },
       ],
     }})
@@ -55,11 +55,123 @@ bot.on("message", message => {
       description: "Earn the Essence Badge by defeating the Grass/Bug Frontier Brain in their Frontier Challenge!",
       fields: [{
         name: "Current Frontier Brain",
-        value: "Jordon / @Jordon."
+        value: "Jordon | @Jordon"
       },
       {
         name: "Frontier Challenge",
         value: "Great League Match | No Fire Type Pokemon or Moves Allowed!"
+      },
+      ],
+    }})
+  }
+  else if(message.content.toLowerCase().indexOf('dynamic') > -1) {
+    message.channel.send({embed: {
+      color: 15844367,
+      title: "Dynamic Badge - Information",
+      description: "Earn the Dynamic Badge by defeating the Electric/Steel Frontier Brain in their Frontier Challenge!",
+      fields: [{
+        name: "Current Frontier Brain",
+        value: "John | @John&Bekka"
+      },
+      {
+        name: "Frontier Challenge",
+        value: "Great League Match | To Be Announced."
+      },
+      ],
+    }})
+  }
+  else if(message.content.toLowerCase().indexOf('revenant') > -1) {
+    message.channel.send({embed: {
+      color: 7419530,
+      title: "Revenant Badge - Information",
+      description: "Earn the Revenant Badge by defeating the Ghost/Poison Frontier Brain in their Frontier Challenge!",
+      fields: [{
+        name: "Current Frontier Brain",
+        value: "Tyler | @SirTyler7"
+      },
+      {
+        name: "Frontier Challenge",
+        value: "Ultra League Match | To Be Announced."
+      },
+      ],
+    }})
+  }
+  else if(message.content.toLowerCase().indexOf('hail') > -1) {
+    message.channel.send({embed: {
+      color: 3447003,
+      title: "Hail Badge - Information",
+      description: "Earn the Hail Badge by defeating the Water/Ice Frontier Brain in their Frontier Challenge!",
+      fields: [{
+        name: "Current Frontier Brain",
+        value: "Easton | @Something Reinhardt"
+      },
+      {
+        name: "Frontier Challenge",
+        value: "Ultra League Match | To Be Announced."
+      },
+      ],
+    }})
+  }
+  else if(message.content.toLowerCase().indexOf('nightmare') > -1) {
+    message.channel.send({embed: {
+      color: 9807270,
+      title: "Nightmare Badge - Information",
+      description: "Earn the Nightmare Badge by defeating the Dark/Fighting Frontier Brain in their Frontier Challenge!",
+      fields: [{
+        name: "Current Frontier Brain",
+        value: "Joe | @Rogue"
+      },
+      {
+        name: "Frontier Challenge",
+        value: "Ultra League Match | To Be Announced."
+      },
+      ],
+    }})
+  }
+  else if(message.content.toLowerCase().indexOf('pyre') > -1) {
+    message.channel.send({embed: {
+      color: 10038562,
+      title: "Pyre Badge - Information",
+      description: "Earn the Pyre Badge by defeating the Fire/Normal Frontier Brain in their Frontier Challenge!",
+      fields: [{
+        name: "Current Frontier Brain",
+        value: "Jamie | @jojopride"
+      },
+      {
+        name: "Frontier Challenge",
+        value: "Master League Match | To Be Announced."
+      },
+      ],
+    }})
+  }
+  else if(message.content.toLowerCase().indexOf('aerial') > -1) {
+    message.channel.send({embed: {
+      color: 1752220,
+      title: "Aerial Badge - Information",
+      description: "Earn the Aerial Badge by defeating the Dragon/Flying Frontier Brain in their Frontier Challenge!",
+      fields: [{
+        name: "Current Frontier Brain",
+        value: "Roger | @RogerAK"
+      },
+      {
+        name: "Frontier Challenge",
+        value: "Master League Match | To Be Announced."
+      },
+      ],
+    }})
+  }
+  else if(message.content.toLowerCase().indexOf('fantasy') > -1) {
+    message.channel.send({embed: {
+      color: 10181046,
+      title: "Fantasy Badge - Information",
+      description: "Earn the Fantasy Badge by defeating the Psychic/Fairy Frontier Brain in their Frontier Challenge!",
+      fields: [{
+        name: "Current Frontier Brain",
+        value: "*???* | @Totalchaos71"
+      },
+      {
+        name: "Frontier Challenge",
+        value: "Master League Match | To Be Announced."
       },
       ],
     }})
